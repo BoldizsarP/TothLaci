601bd2274ba4c2422e276d7afc1d90a5f9067f01
Bob <boldizsarpa@invenshure.com>
2021-07-13 20:41:48 +0200

    Ver1

---
+++ TothLaci.py
@@ -0,0 +1,44 @@
+# import os
+
+# import discord
+
+# TOKEN = os.getenv('DISCORD_TOKEN')
+
+# client = discord.Client()
+
+# @client.event
+# async def on_member_join():
+#     print(f'{client.user} has connected to Discord!')
+
+# client.run(TOKEN)
+import discord
+import time
+from datetime import datetime
+
+TOKEN="ODYzMDk5MzM4OTAwNjM1Njg4.YOh95g.PMTvZv_qxjfGPAJQyCVpZ6IgYQI"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.guilds)
+    text_channels = client.guilds[0].text_channels
+    channel = text_channels[0]
+    # await channel.send("Szasztok skaccok")
+
+@client.event
+async def on_message(message):
+    content = message.content
+    channel = message.channel
+    print(channel.type.value)
+    if channel.type.value == 1:
+        if content.split(' ')[0].isnumeric():
+            time.sleep(int(content.split(' ')[0]))
+            await message.reply(" ".join(content.split(' ')[1:]))
+            
+@client.event
+async def on_member_update(old, new):
+    pass
+
+
+client.run(TOKEN)
